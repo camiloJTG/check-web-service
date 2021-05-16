e851a3b9096727ba8c79e46e09ea79dedeea46e7
camiloJTG <camilojtg.dev@protonmail.com>
2021-05-16 15:58:35 -0400

    feat: Create auth error and validation handler

---
+++ src/controllers/middlewares/handlerValidation.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { SchemaMap, object } from 'joi';
+
+export const handlerValidation = (schema: SchemaMap, check: string) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (check === 'body') {
+      const error = validate(req.body, schema);
+      next(error);
+    }
+    if (check === 'params') {
+      const error = validate(req.params, schema);
+      next(error);
+    }
+  };
+};
+
+export const validate = (data: Request | unknown, schema: SchemaMap) => {
+  const { error } = object(schema).validate(data);
+  return error;
+};
