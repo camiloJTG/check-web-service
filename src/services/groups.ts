7adbd3debb4f9d40128a27253adf2597e72e1194
camiloJTG <camilojtg.dev@protonmail.com>
2021-05-19 00:07:57 -0400

    feat: Create groups and tasks services

---
+++ src/services/groups.ts
@@ -0,0 +1,108 @@
+import { getRepository } from 'typeorm';
+import { Groups } from '../models/groups';
+import { ICreateGroup, IUpdateGroup } from '../interfaces/groups';
+import { getUser } from './users';
+import { deleteByGroupId } from './tasks';
+
+export const createGroup = async (group: ICreateGroup) => {
+  try {
+    // Create repository
+    const groupRepository = getRepository(Groups);
+
+    // Validate if userid is valid
+    const searchUser = await getUser(group.userId);
+    if (typeof searchUser === 'string' || typeof searchUser === 'undefined')
+      return `The id ${group.userId} is not valid`;
+
+    // Set updatedAt
+    group.updatedAt = new Date();
+
+    // Create new group
+    const newGroup = await groupRepository.save(group);
+
+    return newGroup;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+export const getOneGroup = async (id: number) => {
+  try {
+    // Create repository
+    const groupRepository = getRepository(Groups);
+
+    // Validate if id exists
+    const searchGroup = await groupRepository.findOne(id);
+    if (!searchGroup) return `The id ${id} not found`;
+
+    return searchGroup;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+export const getAllGroupByUserId = async (id: number) => {
+  try {
+    // Create repository
+    const groupRepository = getRepository(Groups);
+
+    // Validate if exists data
+    const searchData = await groupRepository.find({ where: { userId: id } });
+    if (searchData.length === 0) return `NO data found`;
+    return searchData;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+export const updateGroup = async (id: number, group: IUpdateGroup) => {
+  try {
+    // Create repository
+    const groupRepository = getRepository(Groups);
+
+    // Search by id
+    const searchId = await groupRepository.findOne(id);
+    if (!searchId) return `The id ${id} not found`;
+
+    if (group.name) {
+      await groupRepository.save({
+        id: id,
+        name: group.name,
+        updatedAt: new Date(),
+      });
+    }
+
+    if (group.isFeatured) {
+      await groupRepository.save({
+        id: id,
+        isFeatured: group.isFeatured,
+        updatedAt: new Date(),
+      });
+    }
+
+    // Find update data
+    const result = await groupRepository.findOne(id);
+    if (!result) return `NO data found`;
+    return result;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+export const deleteGroup = async (id: number) => {
+  try {
+    // Create repository
+    const groupRepository = getRepository(Groups);
+
+    // Validate if id exists
+    const searchGroup = await groupRepository.findOne(id);
+    if (!searchGroup) return `The id ${id} not found`;
+
+    // Delete all tasks
+    await deleteByGroupId(id);
+    await groupRepository.delete(id);
+    return { status: 'ok' };
+  } catch (e) {
+    console.error(e.message);
+  }
+};
---
+++ src/services/tasks.ts
@@ -0,0 +1,131 @@
+import { getRepository } from 'typeorm';
+import { Tasks } from '../models/tasks';
+import { ICreateTasks, IUpdateTask } from '../interfaces/tasks';
+import { getOneGroup } from './groups';
+
+export const createTasks = async (task: ICreateTasks) => {
+  try {
+    // Create repository
+    const taskRepository = getRepository(Tasks);
+
+    // Validate if id is valid
+    const searchId = await getOneGroup(task.groupId);
+    if (typeof searchId === 'string' || typeof searchId === 'undefined')
+      return `The id ${task.groupId} is not valid`;
+
+    // Set updatedAt
+    task.updatedAt = new Date();
+
+    // Create new group
+    const newTask = await taskRepository.save(task);
+
+    return newTask;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+export const getOneTask = async (id: number) => {
+  try {
+    // Create repository
+    const taskRepository = getRepository(Tasks);
+
+    // Valdiate if id is valid
+    const searchTask = await taskRepository.findOne(id);
+    if (!searchTask) return `The id ${id} is not valid`;
+    return searchTask;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+export const getTasksByGroup = async (id: number) => {
+  try {
+    // Create repository
+    const taskRepository = getRepository(Tasks);
+
+    // Valdiate if id is valid
+    const searchTask = await taskRepository.find({ where: { groupId: id } });
+    if (searchTask.length === 0) return `No data found`;
+    return searchTask;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+export const updateTask = async (id: number, task: IUpdateTask) => {
+  try {
+    // Create repository
+    const taskRepository = getRepository(Tasks);
+
+    // Validate if id exists
+    const searchTaskById = await taskRepository.findOne(id);
+    if (!searchTaskById) return `The id ${id} not found`;
+
+    // Update name
+    if (task.name) {
+      await taskRepository.save({
+        id: id,
+        name: task.name,
+        updatedAt: new Date(),
+      });
+    }
+
+    // Update complete
+    if (task.isCompleted) {
+      await taskRepository.save({
+        id: id,
+        isCompleted: task.isCompleted,
+        updatedAt: new Date(),
+      });
+    }
+
+    // Update description
+    if (task.description) {
+      await taskRepository.save({
+        id: id,
+        description: task.description,
+        updatedAt: new Date(),
+      });
+    }
+
+    // Find update data
+    const result = await taskRepository.findOne(id);
+    if (!result) return `NO data found`;
+    return result;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+export const deleteTask = async (id: number) => {
+  try {
+    // Create repository
+    const taskRepository = getRepository(Tasks);
+
+    // Validate if id exists
+    const searchTaskById = await taskRepository.findOne(id);
+    if (!searchTaskById) return `The id ${id} not found`;
+    await taskRepository.delete(id);
+    return { status: 'ok' };
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+export const deleteByGroupId = async (id: number) => {
+  try {
+    // Create repository
+    const taskRepository = getRepository(Tasks);
+
+    // Search id group
+    const searchGroup = await taskRepository.find({ where: { groupId: id } });
+    if (searchGroup.length === 0) return `No data found`;
+
+    // Delete file
+    await taskRepository.delete({ groupId: id });
+    return 1;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
