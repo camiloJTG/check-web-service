820452fce24168da624a0b5e016711ba6741cea8
camiloJTG <camilojtg.dev@protonmail.com>
2021-05-23 00:00:33 -0400

    feat: Add document route

--- src/controllers/routes/index.ts
+++ src/controllers/routes/index.ts
@@ -1,3 +1,5 @@
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from '../../docs/swagger/swagger.json';
 import users from './users';
 import groups from './groups';
 import tasks from './tasks';
@@ -8,6 +10,7 @@ const routes = (app: Application) => {
   app.use(`${prefix}/users`, users);
   app.use(`${prefix}/groups`, groups);
   app.use(`${prefix}/tasks`, tasks);
+  app.use(`${prefix}/docs`, swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 };
 
 export default routes;
